929e0cb8f498384fcc3d0d82012a6ff48dd0fb43
soph-lin <slin12321@gmail.com>
2025-01-20 22:21:11 -0600

    Clean up index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 import PixiApp from "@/lib/components/PixiApp";
-import { RoomProvider } from "@/lib/components/RoomProvider";
+import { GameProvider } from "@/lib/components/GameProvider";
 
 export default function Home() {
   return (
     <div className="min-h-screen items-center justify-items-center gap-16 p-4">
       <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-        <RoomProvider>
+        <GameProvider>
           <PixiApp />
-        </RoomProvider>
+        </GameProvider>
       </main>
       <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6"></footer>
     </div>
